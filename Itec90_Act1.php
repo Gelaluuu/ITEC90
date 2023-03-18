9529286329fa4387d2e367f71a5d1667bd3b84cc
Angel Landicho <angellandicho00@gmail.com>
2023-03-18 11:40:22 +0800

    Activity 1

---
+++ Itec90_Act1.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>WELCOME</title>
+<style>
+body {
+  background-image: url('Blackbg.jpg');
+  background-size: cover;
+}
+
+h1{
+color:White;
+text-align:center;
+font-size:50px;
+}
+h2{
+color:White;
+text-align:center;
+font-size:30px;
+}
+
+h3{
+color:White;
+text-align:center;
+}
+
+</style>
+
+<h1><br><br>HELLO WORLD!</h1>
+<?php
+echo "<h2>Activity 1 in ITEC90- System Integration and Architecture</h2>";
+echo "<h3>Group Members:<br>Atienza, John Philip<br> Bacena, Anthony<br> Catindig, Jaimie<br> Loisaga, Angel<br> Taculod, Reden<br><br>BSIT 4-3</h3>";
+?> 
+</head>
+</body>
+</html>
